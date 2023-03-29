ffa803f4a7f54887cd8e04d5c381c545d3cbc842
adamr714 <adamr714@gmail.com>
2023-03-29 11:19:29 -0700

    Uploading all files

---
+++ precache-manifest.7d63afebf8eb31e98bda4110431d1759.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83847de7ea897107bd5e02e58fba464",
+    "url": "/berggruen_governance_index/index.html"
+  },
+  {
+    "revision": "306d6a9a17a5c7673638",
+    "url": "/berggruen_governance_index/static/css/main.1b104de7.chunk.css"
+  },
+  {
+    "revision": "57dffe46afab7ed88fad",
+    "url": "/berggruen_governance_index/static/js/2.8c11c41a.chunk.js"
+  },
+  {
+    "revision": "306d6a9a17a5c7673638",
+    "url": "/berggruen_governance_index/static/js/main.9e21ce8b.chunk.js"
+  },
+  {
+    "revision": "c32197c7eb8b44b9bb56",
+    "url": "/berggruen_governance_index/static/js/runtime-main.578e515c.js"
+  },
+  {
+    "revision": "0650185914ceaed88805e45ef88432c0",
+    "url": "/berggruen_governance_index/static/media/countrySummary.06501859.csv"
+  },
+  {
+    "revision": "a8b6a863be6f956d7f7965bc23b1ab75",
+    "url": "/berggruen_governance_index/static/media/full_dataset.a8b6a863.csv"
+  },
+  {
+    "revision": "20d92acff8011b27bacc866e3aaa2789",
+    "url": "/berggruen_governance_index/static/media/info.20d92acf.svg"
+  },
+  {
+    "revision": "40d582213752eb9ab4bd50e2c8231d36",
+    "url": "/berggruen_governance_index/static/media/key-first-democracy.40d58221.svg"
+  },
+  {
+    "revision": "b65c62bd780dd89f731efd5fafbd64cd",
+    "url": "/berggruen_governance_index/static/media/key-first-government.b65c62bd.svg"
+  },
+  {
+    "revision": "683d7d8c56afb8086163ac5519c05206",
+    "url": "/berggruen_governance_index/static/media/key-first-life.683d7d8c.svg"
+  },
+  {
+    "revision": "5f65390d10e12f10394e0278a7a808c3",
+    "url": "/berggruen_governance_index/static/media/key-second-democracy.5f65390d.svg"
+  },
+  {
+    "revision": "04f6e388db5daded6a679bb85d5533c0",
+    "url": "/berggruen_governance_index/static/media/key-second-government.04f6e388.svg"
+  },
+  {
+    "revision": "ccf031e7b8171529c906fb670a323432",
+    "url": "/berggruen_governance_index/static/media/key-second-life.ccf031e7.svg"
+  },
+  {
+    "revision": "cbf7841733c80bb5afc3c2de39d8d63b",
+    "url": "/berggruen_governance_index/static/media/keyA.cbf78417.svg"
+  },
+  {
+    "revision": "952860aeb99796aabbf8dfbe3e6da2b6",
+    "url": "/berggruen_governance_index/static/media/keyB.952860ae.svg"
+  },
+  {
+    "revision": "cd30d405080aa96da24021f174cbc41b",
+    "url": "/berggruen_governance_index/static/media/logo-graphic.cd30d405.svg"
+  },
+  {
+    "revision": "5862c2849527328a038742533bdde3cd",
+    "url": "/berggruen_governance_index/static/media/logo.5862c284.svg"
+  },
+  {
+    "revision": "807db33f73d67292c9482013ca299bbb",
+    "url": "/berggruen_governance_index/static/media/reload.807db33f.svg"
+  },
+  {
+    "revision": "5fa037811877e4ceabf58fa548aec735",
+    "url": "/berggruen_governance_index/static/media/scatter-placeholder.5fa03781.svg"
+  }
+]);
